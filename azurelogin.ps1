f5ca0bd4147a4059a64b79e039eb8b742b1eaa15
vinoth69 <consimvinoth@gmail.com>
2018-07-09 17:04:17 +0530

    Update azurelogin.ps1

--- azurelogin.ps1
+++ azurelogin.ps1
@@ -1,6 +1,8 @@
-$azureAccountName = $accname;
-$azurePassword = ConvertTo-SecureString $pwd -AsPlainText -Force
+$azureAccountName ="$accname"
+$azurePassword = ConvertTo-SecureString "$pwd" -AsPlainText -Force
 $psCred = New-Object System.Management.Automation.PSCredential($azureAccountName, $azurePassword)
 #Login-AzureRmAccount -C -Credential $psCred
 
 Add-AzureRmAccount -Credential $psCred -TenantId $tenantid -ServicePrincipal
+
+Get-AzureRmADApplication
